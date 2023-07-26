ae3979b5b04e6aa0519ae7b8617a46ecaf90d4cf
MNR-nengerman <140627726+MNR-nengerman@users.noreply.github.com>
2023-07-26 16:21:59 +0200

    Update script.js
    Added Accordion Functionality

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+    function myFunction(id) {
+      var x = document.getElementById(id);
+      if (x.className.indexOf("store-show") == -1) {
+        x.className += " store-show";
+      } else { 
+        x.className = x.className.replace(" store-show", "");
+      }
+    }
